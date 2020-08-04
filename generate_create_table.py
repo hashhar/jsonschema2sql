45f46ed568f0203c94744b26f36238e2ddef791d
Ashhar Hasan <hashhar_dev@outlook.com>
2020-08-04 17:18:14 +0530

    Generate tables without arrays or structs

--- generate_create_table.py
+++ generate_create_table.py
@@ -2,6 +2,40 @@ import argparse
 import json
 import typing
 
+PROPERTIES_FIELD = "properties"
+TYPE_FIELD = "type"
+REQUIRED_FIELD = "required"
+ITEMS_FIELD = "items"
+FORMAT_FIELD = "format"
+SCALE_FIELD = "scale"
+PRECISION_FIELD = "precision"
+
+# dictionary with keys (json type, format) mapped to sql type
+JSON_TYPE_TO_SQL_TYPE = {
+    # string
+    ("string", None): "varchar",
+    ("string", "date-time"): "timestamp",
+    ("string", "date-time-string"): "varchar",
+    ("string", "date"): "date",
+    ("string", "date-string"): "varchar",
+    ("string", "time"): "time",
+    ("string", "time-string"): "varchar",
+    ("string", "decimal"): "decimal",
+    # number
+    ("number", None): "double",
+    ("number", "double"): "double",
+    ("number", "float"): "float",
+    ("number", "decimal"): "decimal",
+    # integer
+    ("integer", None): "bigint",
+    # boolean
+    ("boolean", None): "boolean",
+    # array
+    ("array", None): "array",
+    # object
+    ("object", None): "row",
+}
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -100,14 +134,33 @@ def generate_create_table(
 
 
 def get_columns(jsonschema: typing.Dict[str, typing.Any]) -> str:
-    return ""
+    columns_sql = ""
+    first = True
+    for field in jsonschema.get(PROPERTIES_FIELD):
+        # all columns except first have trailing comma and newline indent
+        if not first:
+            columns_sql += ",\n    "
+
+        columns_sql += "{}".format(
+            get_column_sql(field, jsonschema.get(PROPERTIES_FIELD).get(field))
+        )
+        first = False
+    return columns_sql
+
+
+def get_column_sql(field: str, jsonschema: typing.Dict[str, typing.Any]) -> str:
+    # create key to lookup matching SQL type
+    key = (jsonschema.get(TYPE_FIELD), jsonschema.get(FORMAT_FIELD, None))
+
+    sql_type = JSON_TYPE_TO_SQL_TYPE.get(key)
+    # TODO: for container SQL types (row and array) we need some more processing
+    return '"{field}" {sql_type}'.format(field=field, sql_type=sql_type)
 
 
 if __name__ == "__main__":
     args = parse_args()
 
     jsonschema = load_schema(args.jsonschema)
-    print(args.partition_columns)
     sql = generate_create_table(
         args.table,
         args.schema,
