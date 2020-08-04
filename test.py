c4e4eb47732a24b0fa06c4152ab2b37804e88ba2
Ashhar Hasan <hashhar_dev@outlook.com>
2020-08-05 01:24:21 +0530

    Add simple test case as an assertion

---
+++ test.py
@@ -0,0 +1,33 @@
+import generate_create_table
+
+jsonschema = generate_create_table.load_schema("test.json")
+sql = generate_create_table.generate_create_table(
+    "test_table", "default", "s3://some-bucket/", ["ad"], "PARQUET", jsonschema
+)
+
+expected_sql = """CREATE TABLE "default"."test_table" (
+    "string_col" varchar,
+    "datetime_col" timestamp,
+    "datetime_string_col" varchar,
+    "date_col" date,
+    "date_string_col" varchar,
+    "time_col" time,
+    "time_string_col" varchar,
+    "decimal_string_col" decimal(10, 2),
+    "double_col" double,
+    "double_double_col" double,
+    "float_col" float,
+    "decimal_col" decimal(5, 3),
+    "action_date" bigint,
+    "boolean_col" boolean,
+    "array_col" array(varchar),
+    "array_object_col" array(ROW("string_col" varchar, "datetime_col" timestamp)),
+    "object_col" ROW("string_col" varchar, "integer_col" bigint)
+) WITH (
+    external_location = 's3://some-bucket/',
+    partitioned_by = ARRAY['ad'],
+    format = 'PARQUET'
+)"""
+
+print(sql)
+assert sql == expected_sql
